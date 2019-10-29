2e05d88e41d29770a80e4008d6e8f3610d3128d4
sharif-uddin1 <sharif.uddin1@vodafone.com>
2019-10-29 09:28:21 +0000

    loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
-        use: ['babel-loader'] 
+        use: {
+            loader: "babel-loader"
+          }
       },
       {
         test: /\.css$/,
